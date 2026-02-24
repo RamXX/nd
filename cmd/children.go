2ef7cf05dca482d8a8925f0cb5e78a16090b3510
RamXX <ramxx@ramirosalas.com>
2026-02-23 20:47:40 -0800

    Add nd children: list child issues of a parent
    Uses the Parent filter from list extensions to show children of any issue.

---
+++ cmd/children.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/RamXX/nd/internal/format"
+	"github.com/RamXX/nd/internal/store"
+	"github.com/spf13/cobra"
+)
+
+var childrenCmd = &cobra.Command{
+	Use:   "children <id>",
+	Short: "List child issues of a parent",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		id := args[0]
+		s, err := store.Open(resolveVaultDir())
+		if err != nil {
+			return err
+		}
+
+		issues, err := s.ListIssues(store.FilterOptions{Parent: id})
+		if err != nil {
+			return err
+		}
+
+		if jsonOut {
+			return format.JSON(os.Stdout, issues)
+		}
+		format.Table(os.Stdout, issues)
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(childrenCmd)
+}
